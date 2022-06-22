a74f2692b96ba89f04bf2546753d45b2537c3ac2
rival <rivaladrianjuliansyah@gmail.com>
2022-06-22 11:54:12 +0700

    Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ParamHasil) => {
+  let Arr1 = await promiseTheaterIXX();
+  let Arr2 = await promiseTheaterVGC();
+  let combineArr = Arr1.concat(Arr1);
+  let countResult = 0;
+  combineArr.map((getData) => getData.hasil === ParamHasil && (countResult += 1));
+  return countResult;
+};
 
 module.exports = {
   promiseOutput,
